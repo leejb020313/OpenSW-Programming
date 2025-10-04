47672120dbe8de17450a643b8fb5f59d27986aa5
leejb020313 <157200431+leejb020313@users.noreply.github.com>
2025-10-05 02:02:21 +0900

    Title: 트리 정점 거리, Time: 43ms, Memory: 16MB, Status: Wrong Answer - Codetree

---
+++ 251005/트리 정점 거리/node-distance.py
@@ -0,0 +1,6 @@
+n, m = map(int, input().split())
+
+edges = [tuple(map(int, input().split())) for _ in range(n - 1)]
+queries = [tuple(map(int, input().split())) for _ in range(m)]
+
+# Please write your code here.
