e1aeaa599cfd22564c00cd92e159d37973570f35
leejb020313 <157200431+leejb020313@users.noreply.github.com>
2025-10-05 02:04:29 +0900

    Title: 트리 간선의 길이, Time: 216ms, Memory: 34MB, Status: Passed - Codetree

--- 251005/트리 간선의 길이/length-of-tree-trunk.py
+++ 251005/트리 간선의 길이/length-of-tree-trunk.py
@@ -1,36 +1,52 @@
+import sys
+sys.setrecursionlimit(100000)
+
 # 변수 선언 및 입력:
-n, k = tuple(map(int, input().split()))
-par = [0] * (n + 1)
-finding_node = -1
-ans = 0
+n = int(input())
+edges = [[] for _ in range(n + 1)]
+visited = [False] * (n + 1)
+dist = [0] * (n + 1)
+max_dist = 0
+last_node = 0
 
-# n개의 수열 원소를 입력받습니다.
-a = [0] + list(map(int, input().split()))
-for i in range(1, n + 1):
-    if a[i] == k: 
-        finding_node = i
+# n - 1개의 간선 정보를 입력받습니다.
+for _ in range(n - 1):
+    x, y, d = tuple(map(int, input().split()))
+    edges[x].append((y, d))
+    edges[y].append((x, d))
 
-# n개의 원소를 바탕으로 트리를 만들어줍니다.
-par_node = 0
-for i in range(2, n + 1):
-    # 두 원소가 연속하지 않는다면
-    # 부모 노드 index를 1 증가시킵니다.
-    if a[i] > a[i - 1] + 1:
-        par_node += 1
+
+# 모든 노드의 정점을 탐색하는 DFS를 진행합니다.
+def dfs(x):
+    global max_dist, last_node
     
-    # i번 노드의 부모 노드는 par_node(index)가 됩니다.
-    par[i] = par_node
+    for y, d in edges[x]:
+        # 이미 방문한 정점이면 스킵합니다.
+        if visited[y]: 
+            continue
 
-# 부모 노드는 다르면서 
-# 부모의 부모 노드가 같은 경우를 찾습니다.
-# 해당 노드들은 사촌이 됩니다.
+        visited[y] = True
+        dist[y] = dist[x] + d
+
+        # 현재 정점을 기준으로 가장 먼 노드를 찾습니다.
+        if dist[y] > max_dist:
+            max_dist = dist[y]
+            last_node = y
+
+        dfs(y)
+
+
+# DFS를 통해 가장 먼 노드를 찾습니다.
+visited[1] = True
+dfs(1)
+
+# 가장 먼 노드에서 시작해 다시 한번 DFS를 돌려 트리의 가장 긴 거리를 찾습니다.
 for i in range(1, n + 1):
-    # 부모의 부모 노드가 존재하지 않는 경우에는 패스합니다.
-    if not par[par[i]] or not par[par[finding_node]]:
-        continue
+    visited[i] = False
+    dist[i] = 0
 
-    if par[i] != par[finding_node] and par[par[i]] == par[par[finding_node]]:
-        ans += 1
+visited[last_node] = True
+dfs(last_node)
 
-# 사촌 노드의 개수를 출력합니다.
-print(ans)
+# 트리의 가장 긴 거리를 출력합니다.
+print(max_dist)
