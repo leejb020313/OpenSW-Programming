df49075e742a8a07999dc8cf33b6b60aa2d1e0eb
leejb020313 <157200431+leejb020313@users.noreply.github.com>
2025-10-05 02:10:19 +0900

    Title: 트리 정점 거리, Time: 135ms, Memory: 19MB, Status: Passed - Codetree

--- 251005/트리 정점 거리/node-distance.py
+++ 251005/트리 정점 거리/node-distance.py
@@ -1,44 +1,50 @@
-# 변수 선언 및 입력:
-n, m = tuple(map(int, input().split()))
-edges = [[] for _ in range(n + 1)]
-visited = [False] * (n + 1)
-dist = [
-    [0] * (n + 1)
-    for _ in range(n + 1)
-]
+import sys
+from collections import deque
 
-# n - 1개의 간선 정보를 입력받습니다.
+# 빠른 입력을 위해 sys.stdin.readline 사용
+input = sys.stdin.readline
+
+# 노드의 개수 N, 거리 질문 개수 M 입력
+n, m = map(int, input().split())
+
+# 트리를 인접 리스트 형태로 표현
+# 각 노드에 (연결된 노드, 거리)를 함께 저장
+graph = [[] for _ in range(n + 1)]
+
+# N-1개의 간선 정보를 입력받아 인접 리스트에 저장
 for _ in range(n - 1):
-    x, y, d = tuple(map(int, input().split()))
+    u, v, w = map(int, input().split())
+    graph[u].append((v, w))
+    graph[v].append((u, w))
 
-    # 간선 정보를 인접리스트에 넣어줍니다.
-    edges[x].append((y, d))
-    edges[y].append((x, d))
-
-
-# DFS를 통해 st로부터 모든 정점까지의 거리를 탐색합니다.
-def dfs(st, x):
-    for y, d in edges[x]:
-        # 이미 방문한 노드는 스킵합니다.
-        if visited[y]: 
-            continue
-        
-        visited[y] = True
-
-        # st로부터의 거리를 갱신합니다.
-        dist[st][y] = dist[st][x] + d
-        dfs(st, y)
-
-
-# 각 n개의 정점에 대해, 모든 노드간의 거리를 DFS로 갱신해줍니다.
-for i in range(1, n + 1):
-    for j in range(1, n + 1): 
-        visited[j] = False
+def get_distance(start, end):
+    """
+    start 노드에서 end 노드까지의 거리를 BFS로 계산하는 함수
+    """
+    # BFS를 위한 큐(deque) 생성
+    # 큐에는 (현재 노드, 시작점부터의 누적 거리)를 저장
+    queue = deque([(start, 0)])
     
-    visited[i] = True
-    dfs(i, i)
+    # 방문 여부를 확인할 리스트
+    visited = [False] * (n + 1)
+    visited[start] = True
+    
+    while queue:
+        # 큐에서 현재 노드와 누적 거리를 꺼냄
+        now, dist = queue.popleft()
+        
+        # 목표 노드에 도달했다면, 거리를 반환하고 함수 종료
+        if now == end:
+            return dist
+        
+        # 현재 노드와 연결된 다른 노드들을 확인
+        for next_node, weight in graph[now]:
+            # 아직 방문하지 않은 노드라면
+            if not visited[next_node]:
+                visited[next_node] = True
+                queue.append((next_node, dist + weight))
 
-# m개의 노드 쌍을 입력받고, 두 노드 쌍 간의 거리를 바로 출력해줍니다.
+# M개의 질문을 처리
 for _ in range(m):
-    x, y = tuple(map(int, input().split()))
-    print(dist[x][y])
+    start_node, end_node = map(int, input().split())
+    print(get_distance(start_node, end_node))
