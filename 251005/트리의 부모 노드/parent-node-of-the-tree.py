7699cbe9da8439f4fbbdd12bd2fbd1af5c629753
leejb020313 <157200431+leejb020313@users.noreply.github.com>
2025-10-05 01:17:39 +0900

    Title: 트리의 부모 노드, Time: 81ms, Memory: 18MB, Status: Runtime Error - Codetree

--- 251005/트리의 부모 노드/parent-node-of-the-tree.py
+++ 251005/트리의 부모 노드/parent-node-of-the-tree.py
@@ -1,5 +1,23 @@
 n = int(input())
 edges = [tuple(map(int, input().split())) for _ in range(n - 1)]
 
-for i in range(0,n-1):
-    print(edges[i][0])
+visited = [False] * (n + 1)
+parent = [0] * (n + 1)
+
+for _ in range(n - 1):
+    x, y = tuple(map(int, input().split()))
+    edges[x].append(y)
+    edges[y].append(x)
+
+def traversal(x):
+    for y in edges[x]:
+        if not visited[y]:
+            visited[y] = True
+            parent[y] = x
+            traversal(y)
+
+visited[1] = True
+traversal(1)
+
+for i in range(2, n + 1):
+    print(parent[i])
